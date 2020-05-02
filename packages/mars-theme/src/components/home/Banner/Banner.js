0bd414e5181a97ade19bfe75f39fc568feb42ac3
Владимир Лучик <volodymyr641@gmail.com>
2020-05-02 18:15:39 +0300

    3 commit

--- packages/mars-theme/src/components/home/Banner/Banner.js
+++ packages/mars-theme/src/components/home/Banner/Banner.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import Button from '../../Button';
+import {styled} from "frontity";
+// import VideoSeatback from '../../../video/sample.mp4'
+
+const Container = styled.div `
+  text-align: left;
+  display:flex;
+  width:80%;
+  line-height:2em;
+  justify-content:space-between;
+  font-family:Montserrat, sans-serif;
+  letter-spacing:0 px;
+  opacity:1;
+`;
+const Discription = styled.p`
+  text-align: left;
+  font-family:Montserrat,sans-serif;
+  letter-spacing: 0 px;
+  height:70px;
+  line-height:1.3em;
+  color:#A09D9D;
+  opacity:1;
+  color:#A09D9D;
+`;
+const Video = styled.div `
+  position: absolute;
+  right: 0;
+  top: 150px;
+`;
 
 const Banner = () => {
   return (
+    <Container>
     <div className='main-block'>
       <div className='slogan'>
         <h2>Make work dynamic,<br />
         take wellness to <br />
         <span className='kw'>new heights</span></h2>
       </div>
-      <div className='picture'>
-        <img src="" alt="coder"/>
-      </div>
-
-      <div className='discription'>
-        <h5>seatback is a holistic welness system that motivates your<br /> 
+      <Discription>
+        <h5>Seatback is a holistic welness system that motivates your<br /> 
         team to stay healthy by setting and achieving health<br /> 
         goals and improving sitting quality
         </h5>
-      </div>
-      
+      </Discription>
         <Button> Get a demo </Button>
-      
     </div>
+    <video autoPlay><source src="https://seatback-admin.webbuilder.in.ua/wp-content/uploads/2020/05/sample.mp4?_=1"
+    type="video/mp4"/></video>
+    </Container>
   );
 }
-
 
 export default Banner;
