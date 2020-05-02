973b4b9de6d53cfd765f88e3eb19e55f13950596
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-05-02 15:37:26 +0300

    description + main styles

--- packages/mars-theme/src/components/home/Description/Description.js
+++ packages/mars-theme/src/components/home/Description/Description.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from "frontity";
 import ImageSeatbackPlus from '../../../img/seatback_plus@2x.png';
 import Button from '../../Button';
+import Link from "../../link";
 
 const Container = styled.div`
   background: #F5F6FA 0% 0% no-repeat padding-box;
-  padding-top: 105px;
-  padding-bottom: 105px;
+  padding-top: 205px;
+  padding-bottom: 205px;
   position: relative;
+  padding-left: ${props => props.alignImage === 'left' ? '50%' : 0};
+
 `;
 
 const Title = styled.h2`
@@ -52,30 +55,85 @@ const Action = styled.div`
 
 const Image = styled.div`
   position: absolute;
-  right: 0;
-  top: 150px;
+  width: 50%;
+  height: 100%;
+  right: ${props => props.alignImage === 'left' ? 'auto' : 0};
+  top: 0;
+  left: ${props => props.alignImage === 'left' ? 0 : 'auto'};
+  display: flex;
+  align-items: center;
+  justify-content: ${props => props.alignImage === 'left' ? 'flex-start' : 'flex-end'};
 `;
 
 
+const data = [
+  {
+    title: 'Seatback+',
+    subtitle: 'An integrated wellness platform.',
+    description: 'That uses AI and Machine Learning to help improve health outcomes. Seatback+ is home to Wellness on Demand, a onestop shop for personalized recommendations, advice for healthy workspace calibration, posture enhancement tips, wellness content, on-demand health classes and real-time personal training.',
+    image: ImageSeatbackPlus,
+    alignImage: 'right',
+    link: '/',
+    linkText: 'Learn more',
+  },
+  {
+    title: 'Office Chair',
+    subtitle: 'A new frontier for any seating device.',
+    description: 'Seatback embeds proprietary technology directly into office chairs. Our revolutionary seating technology guides users through more dynamic workdays and helps them achieve their best posture.',
+    image: ImageSeatbackPlus,
+    alignImage: 'left',
+    link: '/',
+    linkText: 'Learn more',
+  },
+  {
+    title: 'Portable Device',
+    subtitle: 'A flexible solution for any office environment.',
+    description: 'Our proprietary technology can also be embedded directly into a portable device that can be easily shifted to any workstation.',
+    image: ImageSeatbackPlus,
+    alignImage: 'right',
+    link: '/abouts',
+    linkText: 'Learn more',
+  },
+  {
+    title: 'Smart Watch',
+    subtitle: 'Real-time feedback, measurable results.',
+    description: 'Seatback integrates with compatible smart watches to give personalized recommendations and keep users on track to achieve their daily health goals.',
+    image: ImageSeatbackPlus,
+    alignImage: 'left',
+    link: '/abouts',
+    linkText: 'Learn more',
+  },
+];
 const Description = () => {
+
   return (
-    <Container>
-      <Title>
-        Seatback+
-      </Title>
-      <Image>
-        <img src={ImageSeatbackPlus} width={660} alt="image" />
-      </Image>
-      <SubTitle>
-        An integrated wellness platform.
-      </SubTitle>
-      <DescriptionParagraph>
-        That uses AI and Machine Learning to help improve health outcomes. Seatback+ is home to Wellness on Demand, a onestop shop for personalized recommendations, advice for healthy workspace calibration, posture enhancement tips, wellness content, on-demand health classes and real-time personal training.
-      </DescriptionParagraph>
-      <Action>
-        <Button>Learn more</Button>
-      </Action>
-    </Container>
+    <>
+      {data.map(d => {
+        return (
+          <Container alignImage={d.alignImage}>
+            <Title>
+              {d.title}
+            </Title>
+            <Image alignImage={d.alignImage}>
+              <img src={d.image} width={660} alt="image" />
+            </Image>
+            <SubTitle>
+              {d.subtitle}
+            </SubTitle>
+            <DescriptionParagraph>
+              {d.description}
+            </DescriptionParagraph>
+            <Action>
+              {d.link && d.link !== '' && (
+                <Link link={d.link}>
+                  <Button>{d.linkText}</Button>
+                </Link>
+              )}
+            </Action>
+          </Container>
+        );
+      })}
+    </>
   );
 }
 
