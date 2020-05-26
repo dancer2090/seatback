2436e5eaa0f6b7b5993e07ea0787716cb1095b3f
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-05-26 17:10:14 +0300

    404 page added

---
+++ packages/mars-theme/src/components/404/Page404.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../Button";
+import Link from "../link";
+
+import { GlobalContainer, Container, Slogan, Discription } from "./styles";
+
+const Page404 = ({ title, description }) => {
+  return (
+    <GlobalContainer>
+      <Container>
+        <Slogan>{title}</Slogan>
+        <Discription>{description}</Discription>
+        <Link link="/">
+          <Button>Go to the main page</Button>
+        </Link>
+      </Container>
+    </GlobalContainer>
+  );
+};
+export default Page404;
--- packages/mars-theme/src/components/page-error.js
+++ packages/mars-theme/src/components/page-error.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Page404Content from './404';
 import { styled, connect } from "frontity";
 
 const description404 = (
@@ -21,37 +22,16 @@ const description = (
 // The 404 page component
 const Page404 = ({ state }) => {
   const data = state.source.get(state.router.link);
-
   const title = "Oops! Something went wrong";
   const title404 = "Oops! 404";
 
   return (
-    <Container>
-      <Title>{data.is404 ? title404 : title}</Title>
-      <Description>{data.is404 ? description404 : description}</Description>
-    </Container>
+    <Page404Content 
+      title={data.is404 ? title404 : title}
+      description={data.is404 ? description404 : description}
+    />
   );
 };
 
 export default connect(Page404);
 
-const Container = styled.div`
-  width: 800px;
-  margin: 0;
-  padding: 24px;
-  text-align: center;
-`;
-
-const Title = styled.h1`
-  margin: 0;
-  margin-top: 24px;
-  margin-bottom: 8px;
-  color: rgba(12, 17, 43);
-  font-size: 4em;
-`;
-
-const Description = styled.div`
-  line-height: 1.6em;
-  color: rgba(12, 17, 43, 0.8);
-  margin: 24px 0;
-`;
