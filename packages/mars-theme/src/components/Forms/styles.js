ab4f10b8002c8823d96091402ede157e295be08a
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-05-27 23:08:28 +0300

    eslint form fix

--- packages/mars-theme/src/components/Forms/styles.js
+++ packages/mars-theme/src/components/Forms/styles.js
@@ -1,16 +1,13 @@
+/* eslint-disable no-nested-ternary */
 import { styled, css } from 'frontity';
 import {
-  SIZE_DESCTOP_MEDIUM_2,
   SIZE_DESCTOP_MEDIUM_1,
   SIZE_DESCTOP_SMALL,
-  SIZE_LAPTOP,
-  SIZE_LAPTOP_SMALL,
   SIZE_MOBILE,
   TEXT_FONT_SIZE_MOBILE,
   TEXT_LINE_HEIGHT_MOBILE,
   TEXT_FONT_SIZE_DESCTOP_MEDIUM_1,
   TEXT_LINE_HEIGHT_DESCTOP_MEDIUM_1,
-  SIZE_DESCTOP_LARGE,
   HEADER_FONT_SIZE_DESCTOP_MEDIUM_1,
   HEADER_LINE_HEIGHT_DESCTOP_MEDIUM_1,
   HEADER_FONT_SIZE_MOBILE,
@@ -18,7 +15,7 @@ import {
   getPxSize,
 } from '../../const/responsive';
 
-export const Main = styled.div `
+export const Main = styled.div`
   padding-top:123px;
   margin-bottom:123px;
   position: relative;
@@ -38,7 +35,7 @@ export const Main = styled.div `
   }
   &:before{
     content: ' ';
-    background-color: ${props => props.bgColor ? props.bgColor : '#F5F6FA'};
+    background-color: ${(props) => (props.bgColor ? props.bgColor : '#F5F6FA')};
     width: 100%;
     height: 1272px;
     position: absolute;
@@ -49,16 +46,16 @@ export const Main = styled.div `
 `;
 
 export const CustomSelectlStyles = {
-  option: (provided, state) => ({
+  option: (provided) => ({
     ...provided,
-    "@media only screen and (max-width: 1440px)": {
+    '@media only screen and (max-width: 1440px)': {
       fontSize: '16px',
     },
-    "@media only screen and (max-width: 600px)": {      
+    '@media only screen and (max-width: 600px)': {
       fontSize: '13px',
     },
   }),
-  container: (provided, state) => ({
+  container: (provided) => ({
     ...provided,
     outline: 'none',
     display: 'flex',
@@ -69,9 +66,6 @@ export const CustomSelectlStyles = {
     lineHeight: '24px',
     letterSpacing: 0,
     color: '#A09D9D',
-    opacity: 1,
-    outline: 'none',
-    // backgroundColor: 'white',
     border: 'none',
     borderColor: '52DE97',
     transition: 'all 0.3s ease-in-out',
@@ -85,7 +79,7 @@ export const CustomSelectlStyles = {
     boxShadow: 'none !important',
     backgroundColor: state.isFocused ? '#fffce3' : state.selectProps.error ? '#ffebeb' : 'white',
   }),
-  valueContainer: (provided, state) => ({
+  valueContainer: (provided) => ({
     ...provided,
     display: 'flex',
     alignItems: 'center',
@@ -97,19 +91,18 @@ export const CustomSelectlStyles = {
     lineHeight: '24px',
     letterSpacing: 0,
     color: '#A09D9D',
-    opacity: 1,
     paddingLeft: '32px',
     outline: 'none',
     backgroundColor: 'transparent',
     transition: 'all 0.3s ease-in-out',
-    "@media only screen and (max-width: 1440px)": {
+    '@media only screen and (max-width: 1440px)': {
       fontSize: `${TEXT_FONT_SIZE_DESCTOP_MEDIUM_1}px`,
       lineHeight: `${TEXT_LINE_HEIGHT_DESCTOP_MEDIUM_1}px`,
       height: '60px',
       paddingLeft: '25px',
       paddingRight: '25px',
     },
-    "@media only screen and (max-width: 600px)": {
+    '@media only screen and (max-width: 600px)': {
       fontSize: `${TEXT_FONT_SIZE_MOBILE}px`,
       lineHeight: `${TEXT_LINE_HEIGHT_MOBILE}px`,
       height: '45px',
@@ -117,7 +110,7 @@ export const CustomSelectlStyles = {
       paddingRight: '20px',
     },
   }),
-  placeholder: (provided, state) => ({
+  placeholder: (provided) => ({
     ...provided,
     height: '100%',
     top: 0,
@@ -132,18 +125,18 @@ export const CustomSelectlStyles = {
   indicatorsContainer: () => ({
     paddingRight: '40px',
     transform: 'scale(1.5)',
-    "@media only screen and (max-width: 1440px)": {
+    '@media only screen and (max-width: 1440px)': {
       paddingRight: '25px',
       transform: 'scale(1.2)',
     },
-    "@media only screen and (max-width: 600px)": {      
+    '@media only screen and (max-width: 600px)': {
       paddingRight: '10px',
       transform: 'scale(1)',
     },
   }),
-}
+};
 
-export const Title = styled.h2 `
+export const Title = styled.h2`
   text-align: center;
   font-size: 50px;
   line-height: 70px;
@@ -170,11 +163,11 @@ export const Title = styled.h2 `
     line-height: ${HEADER_LINE_HEIGHT_MOBILE}px;
   }
 `;
-export const FormContainer = styled.form `
+export const FormContainer = styled.form`
   max-width: 954px;
   margin:0 auto;
   position: relative;
-  ${props => props.preload && css `
+  ${(props) => props.preload && css`
     &:after{
       content: ' ';
       position: absolute;
@@ -187,7 +180,7 @@ export const FormContainer = styled.form `
   `}
 `;
 
-export const FText = styled.div `
+export const FText = styled.div`
   margin-top: 50px;
   overflow: hidden;
   position: relative;
@@ -197,16 +190,16 @@ export const FText = styled.div `
   font-size:0px;
   opacity: 0;
 
-  ${props => props.afterload && css `
+  ${(props) => props.afterload && css`
     font-size: 16px;
     opacity: 1;
   `}
 `;
-export const FormControl = styled.div `
+export const FormControl = styled.div`
   margin-top:60px;
   align-items: center;
 
-  ${props => props.error && css `
+  ${(props) => props.error && css`
     label {
       color: red;
       span {
@@ -227,7 +220,7 @@ export const FormControl = styled.div `
   }
 `;
 
-export const FormLabel = styled.label `
+export const FormLabel = styled.label`
   text-align: left;
   font-weight: 500;
   font-size: 20px;
@@ -253,7 +246,7 @@ export const FormLabel = styled.label `
   }
 `;
 
-export const FormInput = styled.input `
+export const FormInput = styled.input`
   height: 88px;
   width: 100%;
   border: 1px solid #52DE97;
@@ -307,7 +300,7 @@ export const FormInput = styled.input `
   }
 `;
 
-export const FormShare = styled.div `
+export const FormShare = styled.div`
   height: 176px;
   width: 954px;
   border: 1px solid #52DE97;
@@ -321,7 +314,7 @@ export const FormShare = styled.div `
   padding-top:32px;
   padding-left:32px;
 `;
-export const BBlock = styled.div `
+export const BBlock = styled.div`
   margin:0 auto;
   width:219px;
   padding-top: 60px;
