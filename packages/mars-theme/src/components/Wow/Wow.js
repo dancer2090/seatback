780c6e7b205dda21d42136e71e3cc6d69401e346
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-05-27 21:54:30 +0300

    wow js fix

--- packages/mars-theme/src/components/Wow/Wow.js
+++ packages/mars-theme/src/components/Wow/Wow.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactWOW from 'react-wow';
 import { connect } from 'frontity';
 
-const Wow = ({ state, actions, libraries, offset, iteration, ...props}) => {
-  return (
-    <ReactWOW iteration={iteration ? iteration : '1'} offset={!state.seatbackapi.isWow ? 0 : offset} disabled={!state.seatbackapi.isWow} {...props} />
-  )
-}
+const Wow = ({
+  state, actions, libraries, offset, iteration, ...props
+}) => (
+  <ReactWOW
+    iteration={iteration || '1'}
+    offset={!state.seatbackapi.isWow ? 0 : offset}
+    disabled={!state.seatbackapi.isWow}
+    {...props}
+  />
+);
 
 export default connect(Wow);
