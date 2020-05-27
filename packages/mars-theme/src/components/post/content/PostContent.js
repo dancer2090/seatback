8190e19bb93ee1f7827591c9916b1c516bf3cde0
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-05-27 22:42:24 +0300

    post content fix

--- packages/mars-theme/src/components/post/content/PostContent.js
+++ packages/mars-theme/src/components/post/content/PostContent.js
@@ -1,32 +1,28 @@
 import React from 'react';
-import { connect } from "frontity";
+import { connect } from 'frontity';
 import StandartTemplate from '../Templates/StardartTemplate';
 import OurStoryTemplate from '../Templates/OurStoryTemplate';
 import OurPlansTemplate from '../Templates/OurPlansTemplate';
 import SpecialTemplate from '../Templates/SpecialTemplate';
 import ProductTemplate from '../Templates/ProductTemplate';
 import EnterpriceTemplate from '../Templates/EnterpriceTemplate';
-
-import { 
-  GlobalContainer,
-} from './styles';
+import { GlobalContainer } from './styles';
 
 
 const PostContent = ({ state }) => {
-
-  const data_p = state.source.get(state.router.link);
-  const post = state.source[data_p.type][data_p.id];
+  const dataP = state.source.get(state.router.link);
+  const post = state.source[dataP.type][dataP.id];
   const template = (post.template !== '' ? post.template : 'standart');
 
   return (
     <GlobalContainer>
-      {template=="standart" && <StandartTemplate />}
-      {template=="page-our_story.php" && <OurStoryTemplate />}
-      {template=="page-special_page.php" && <SpecialTemplate />}
-      {template=="page-plan.php" && <OurPlansTemplate />}   
-      {template=="page-product.php" && <ProductTemplate />}
-      {template=="page-enterprice.php" && <EnterpriceTemplate />}
+      {template === 'standart' && <StandartTemplate />}
+      {template === 'page-our_story.php' && <OurStoryTemplate />}
+      {template === 'page-special_page.php' && <SpecialTemplate />}
+      {template === 'page-plan.php' && <OurPlansTemplate />}
+      {template === 'page-product.php' && <ProductTemplate />}
+      {template === 'page-enterprice.php' && <EnterpriceTemplate />}
     </GlobalContainer>
   );
-}
+};
 export default connect(PostContent);
